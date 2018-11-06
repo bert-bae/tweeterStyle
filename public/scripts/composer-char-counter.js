c1dd8fcdf755e467dd9bb3ffb4784c5ae0082c8b
Elbert <elbert.bae@gmail.com>
2018-11-06 16:58:40 +0000

    adjusted keypress to keyup to address copy paste issue. Assigned CSS change to character count once it is below 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
-  $('#submit-tweet').on('keypress', function () {
+  $('#submit-tweet').on('keyup', function () {
     // console.log(this.value.length + 1); //counts length of input
-    let maxCount = Number($(this).siblings('span.counter').text())
+    let count = 140;
+    let length = this.value.length;
+    let maxCount = Number($(this).siblings('span.counter').text());
     if (maxCount < 0) {
       // polish up this error sign, maybe a hidden error that pops up would be better
-      $(this).siblings('.tweet-submit').css({
-        "background-color": "red",
-        "color": "yellow",
+      $(this).siblings('span.counter').css({
+        "color": "red",
       });
     } else {
-      maxCount--;
+      maxCount = count - length;
     }
     $('span.counter').text(maxCount);
   });
